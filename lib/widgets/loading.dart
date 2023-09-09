226f28498fc02310346c03c314ae446e9d4d2f98
Pham Van Hung <120079061+Hungpham16012@users.noreply.github.com>
2023-09-09 08:11:33 +0700

    sang 9/9

--- lib/widgets/loading.dart
+++ lib/widgets/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+// ignore: must_be_immutable
 class LoadingWidget extends StatelessWidget {
   double height = 200;
   LoadingWidget({super.key, required this.height});
