c8afac16b5c7719b2b0a2b859c4a5c12e9cc8d2e
tvntran1310 <truongvntran@gmail.com>
2023-09-13 10:45:25 +0700

    [home-page] add scan qr code feature

--- lib/pages/home/main.dart
+++ lib/pages/home/main.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:easy_autocomplete/easy_autocomplete.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter_datawedge/flutter_datawedge.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ghethanhpham_thaco/blocs/app_bloc.dart';
 import 'package:ghethanhpham_thaco/blocs/scan_bloc.dart';
@@ -27,8 +27,8 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   static RequestHelper requestHelper = RequestHelper();
-  late AppBloc _ab;
-  late ScanBloc _sb;
+  late AppBloc _appBloc;
+  late ScanBloc _scanBloc;
   String _qrData = '';
   final _qrDataController = TextEditingController();
   Timer? _debounce;
@@ -40,32 +40,31 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    _ab = Provider.of<AppBloc>(context, listen: false);
-    _sb = Provider.of<ScanBloc>(context, listen: false);
-    FlutterDataWedge dataWedge =
-        FlutterDataWedge(profileName: 'thghethanhpham');
+    _appBloc = Provider.of<AppBloc>(context, listen: false);
+    _scanBloc = Provider.of<ScanBloc>(context, listen: false);
+  }
 
-// Khởi tạo Stream để lắng nghe kết quả quét
-    dataWedge.onScanResult.listen((result) {
+  Future<void> _scanQRCode() async {
+    try {
+      final result = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666", // Màu của đường scan
+        "Hủy", // Văn bản nút hủy
+        true, // Hiển thị biểu tượng flash
+        ScanMode.QR, // Chế độ quét (QR hoặc BARCODE)
+      );
+
+      if (!mounted) return;
+
       setState(() {
-        _qrData = '';
-        _qrDataController.text = '';
-        _data = null;
-        Future.delayed(const Duration(seconds: 1), () {
-          _qrData = result.data;
-          _qrDataController.text = result.data;
-          _onScan(result.data);
-        });
+        _qrData = result;
       });
-    });
 
-// Khởi tạo Stream để lắng nghe trạng thái của máy quét (nếu cần)
-    dataWedge.onScannerStatus.listen((status) {
-      // Xử lý trạng thái máy quét ở đây nếu cần
-    });
-
-// Bật máy quét
-    dataWedge.activateScanner(true);
+      // Gọi hàm tùy chỉnh xử lý dữ liệu quét
+      _onScan(_qrData);
+    } catch (e) {
+      // Xử lý lỗi nếu có
+      print("Lỗi khi quét mã QR: $e");
+    }
   }
 
   void _onSearchChanged(String query) {
@@ -104,40 +103,47 @@ class _MainPageState extends State<MainPage> {
     }
   }
 
+  // Hàm tùy chỉnh để xử lý dữ liệu quét
   _onScan(value) {
     setState(() {
       _loading = true;
     });
-    _sb.getData(value, _ab.isNhapKho).then((_) {
-      setState(() {
-        _qrData = value;
-        if (_sb.data == null) {
-          _qrData = '';
-          _qrDataController.text = '';
-          if (_sb.success == false && _sb.message!.isNotEmpty) {
-            openSnackBar(context, _sb.message!);
-          } else {
-            openSnackBar(context, "Không có dữ liệu");
-          }
-        }
-        _loading = false;
-        _data = _sb.data;
-      });
-    });
+    _scanBloc.getData(value, _appBloc.isNhapKho).then(
+      (_) {
+        setState(
+          () {
+            _qrData = value;
+            if (_scanBloc.data == null) {
+              _qrData = '';
+              _qrDataController.text = '';
+              if (_scanBloc.success == false && _scanBloc.message!.isNotEmpty) {
+                openSnackBar(context, _scanBloc.message!);
+              } else {
+                openSnackBar(context, "Không có dữ liệu");
+              }
+            }
+            _loading = false;
+            _data = _scanBloc.data;
+          },
+        );
+      },
+    );
   }
 
   _onSave() {
-    setState(() {
-      _loading = true;
-    });
-    _data!.chuyenId = _ab.chuyenId!;
+    setState(
+      () {
+        _loading = true;
+      },
+    );
+    _data!.chuyenId = _appBloc.chuyenId!;
     // call api
     AppService().checkInternet().then((hasInternet) {
       if (!hasInternet!) {
         openSnackBar(context, 'no internet'.tr());
       } else {
-        _sb.postData(_data!).then((_) {
-          if (_sb.success) {
+        _scanBloc.postData(_data!).then((_) {
+          if (_scanBloc.success) {
             openSnackBar(context, "Lưu thành công");
           } else {
             openSnackBar(context, "Lưu thất bại");
@@ -155,7 +161,7 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (_ab.tenNhomChucNang == null) {
+    if (_appBloc.tenNhomChucNang == null) {
       return const Center(
           child: Text(
         "Bạn chưa cấu hình để sử dụng.",
@@ -174,47 +180,54 @@ class _MainPageState extends State<MainPage> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
-                    _ab.tenChuyen!,
+                    _appBloc.tenChuyen!,
                     style: Theme.of(context).textTheme.titleLarge,
-                  ).tr(),
+                  ),
                   const SizedBox(height: 10),
                   const DividerWidget(),
                   const SizedBox(height: 10),
                   EasyAutocomplete(
-                      controller: _qrDataController,
-                      onChanged: _onSearchChanged,
-                      suggestions: _results,
-                      onSubmitted: _onScan,
-                      decoration: InputDecoration(
-                        hintText: 'Enter QR/Bar code',
-                        contentPadding: const EdgeInsets.symmetric(
-                          vertical: 0,
-                          horizontal: 10,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                            borderSide: const BorderSide(
-                              color: Colors.purple,
-                              style: BorderStyle.solid,
-                            )),
+                    controller: _qrDataController,
+                    onChanged: _onSearchChanged,
+                    suggestions: _results,
+                    onSubmitted: _onScan,
+                    decoration: InputDecoration(
+                      hintText: 'Enter QR/Bar code',
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 0,
+                        horizontal: 10,
                       ),
-                      suggestionBuilder: (data) {
-                        return Container(
-                          margin: const EdgeInsets.all(1),
-                          padding: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColor,
-                            borderRadius: BorderRadius.circular(5),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(
+                          color: Colors.purple,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                    ),
+                    suggestionBuilder: (data) {
+                      return Container(
+                        margin: const EdgeInsets.all(1),
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Text(
+                          data,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
                           ),
-                          child: Text(
-                            data,
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                          ),
-                        );
-                      }),
+                        ),
+                      );
+                    },
+                  ),
                   const SizedBox(height: 10),
+                  ElevatedButton.icon(
+                    onPressed: _scanQRCode,
+                    icon: const Icon(Icons.camera),
+                    label: const Text('Quét mã'),
+                  ),
                 ],
               ),
             ),
@@ -239,12 +252,13 @@ class _MainPageState extends State<MainPage> {
                             if (_data!.ngay != null)
                               // if (_data!.ngay != null)
                               SizedBox(
-                                  child: Column(
-                                children: [
-                                  showInfoXe("Ngày", _data!.ngay.toString()),
-                                  const SizedBox(height: 10),
-                                ],
-                              )),
+                                child: Column(
+                                  children: [
+                                    showInfoXe("Ngày", _data!.ngay.toString()),
+                                    const SizedBox(height: 10),
+                                  ],
+                                ),
+                              ),
                           ],
                         ),
                       ),
@@ -266,7 +280,7 @@ class _MainPageState extends State<MainPage> {
                         color: Theme.of(context).colorScheme.onPrimary,
                       ),
                       label: Text(
-                        _ab.isNhapKho
+                        _appBloc.isNhapKho
                             ? (_data!.nhapXuatKhoId == null
                                 ? "Nhập kho"
                                 : "Huỷ xác nhận")
