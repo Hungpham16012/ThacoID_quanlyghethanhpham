5957f1c911a32275b41efe812afee77b8793d3f1
tvntran1310 <truongvntran@gmail.com>
2023-09-14 15:47:34 +0700

    resolve conflict

--- lib/pages/home/main.dart
+++ lib/pages/home/main.dart
@@ -4,8 +4,8 @@ import 'dart:convert';
 import 'package:easy_autocomplete/easy_autocomplete.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-// import 'package:flutter_datawedge/flutter_datawedge.dart';
-import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ghethanhpham_thaco/blocs/app_bloc.dart';
 import 'package:ghethanhpham_thaco/blocs/scan_bloc.dart';
 import 'package:ghethanhpham_thaco/models/scan.dart';
@@ -14,9 +14,9 @@ import 'package:ghethanhpham_thaco/services/request_helper.dart';
 import 'package:ghethanhpham_thaco/ultis/snackbar.dart';
 import 'package:ghethanhpham_thaco/widgets/divider.dart';
 import 'package:ghethanhpham_thaco/widgets/loading.dart';
+
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -27,94 +27,45 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   static RequestHelper requestHelper = RequestHelper();
-  late AppBloc _ab;
-  late ScanBloc _sb;
+  late AppBloc _appBloc;
+  late ScanBloc _scanBloc;
   String _qrData = '';
   final _qrDataController = TextEditingController();
   Timer? _debounce;
   List<String>? _results = [];
   ScanModel? _data;
-  List<ScanModel> scannedProducts = [];
+  List<ScanModel> listScannedProduct = [];
 
   bool _loading = false;
 
   @override
   void initState() {
     super.initState();
-    _ab = Provider.of<AppBloc>(context, listen: false);
-    _sb = Provider.of<ScanBloc>(context, listen: false);
-    // FlutterDataWedge.initScanner(
-    //   profileName: 'thghethanhpham',
-    //   onScan: (result) {
-    //     setState(() {
-    //       _qrData = '';
-    //       _qrDataController.text = '';
-    //       _data = null;
-    //       Future.delayed(const Duration(seconds: 1), () {
-    //         _qrData = result.data;
-    //         _qrDataController.text = result.data;
-    //         _onScan(result.data);
-    //       });
-    //     });
-    //   },
-    //   onStatusUpdate: (result) {},
-    // );
+    _appBloc = Provider.of<AppBloc>(context, listen: false);
+    _scanBloc = Provider.of<ScanBloc>(context, listen: false);
   }
 
-  // Future<void> _scanQRCode() async {
-  //   try {
-  //     final result = await FlutterBarcodeScanner.scanBarcode(
-  //       "#ff6666", // Màu của đường scan
-  //       "Hủy", // Văn bản nút hủy
-  //       true, // Hiển thị biểu tượng flash
-  //       ScanMode.QR, // Chế độ quét (QR hoặc BARCODE)
-  //     );
+  Future<void> _scanQRCode() async {
+    try {
+      final result = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666', // Color of the scan line
+        'Hủy', // text button
+        true, // show flash icon
+        ScanMode.QR, // scan feature (QR or BARCODE)
+      );
 
-  //     if (!mounted) return;
+      if (!mounted) return;
 
-  //     setState(() {
-  //       _qrData = result;
-  //       scannedProducts.add(_sb.data!);
-  //     });
-
-  //     // Gọi hàm tùy chỉnh xử lý dữ liệu quét
-  //     _onScan(_qrData);
-  //   } catch (e) {
-  //     // Xử lý lỗi nếu có
-  //     print("Lỗi khi quét mã QR: $e");
-  //   }
-  // }
-
-  // Hàm tùy chỉnh để xử lý dữ liệu quét
-  _onScan(value) {
-    setState(() {
-      _loading = true;
-    });
-
-    _sb.getData(value, _ab.isNhapKho).then((_) {
       setState(() {
-        _qrData = value;
-
-        if (_sb.data == null) {
-          _qrData = '';
-          _qrDataController.text = '';
-          if (_sb.success == false && _sb.message!.isNotEmpty) {
-            openSnackBar(context, _sb.message!);
-          } else {
-            openSnackBar(context, "Không có dữ liệu");
-          }
-        } else {
-          // Add the scanned product to the list
-
-          // Clear the scanned data
-          _qrData = '';
-          _qrDataController.text = '';
-        }
-
-        _loading = false;
-        _data = _sb.data;
+        _qrData = result;
       });
-    });
+
+      // call resolve scan data function
+      _onScan(_qrData);
+    } catch (e) {
+      // print error
+      print("Lỗi khi quét mã QR: $e");
+    }
   }
 
   void _onSearchChanged(String query) {
@@ -153,19 +104,48 @@ class _MainPageState extends State<MainPage> {
     }
   }
 
-  _onSave() {
+  _onScan(qrCode) {
     setState(() {
       _loading = true;
     });
-    //_data!.chuyenId = _ab.chuyenId;
+
+    _scanBloc.getData(qrCode, _appBloc.isNhapKho).then(
+      (_) {
+        setState(
+          () {
+            _qrData = _qrData;
+            if (_scanBloc.data == null) {
+              _qrData = '';
+              _qrDataController.text = '';
+              if (_scanBloc.success == false && _scanBloc.message!.isNotEmpty) {
+                openSnackBar(context, _scanBloc.message!);
+              } else {
+                openSnackBar(context, "Không có dữ liệu");
+              }
+            }
+            _loading = false;
+            _data = _scanBloc.data;
+          },
+        );
+      },
+    );
+  }
+
+  _onSave() {
+    setState(
+      () {
+        _loading = true;
+      },
+    );
+    _data!.chuyenId = _appBloc.chuyenId!;
     // call api
     AppService().checkInternet().then((hasInternet) {
       if (!hasInternet!) {
         openSnackBar(context, 'no internet'.tr());
       } else {
-        _sb.postData(_data!).then((_) {
-          if (_sb.success) {
-            openSnackBar(context, "Lưu thành công");
+        _scanBloc.postData(_data!).then((_) {
+          if (_scanBloc.success) {
+            openSnackBar(context, 'Lưu thành công');
           } else {
             openSnackBar(context, "Lưu thất bại");
           }
@@ -182,32 +162,32 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (_ab.tenNhomChucNang == null) {
+    if (_appBloc.tenNhomChucNang == null) {
       return const Center(
           child: Text(
         "Bạn chưa cấu hình để sử dụng.",
         style: TextStyle(fontSize: 20),
       ));
-    }
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 5),
-          Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            color: Theme.of(context).colorScheme.onPrimary,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  _ab.tenChuyen!,
-                  style: Theme.of(context).textTheme.titleLarge,
-                ).tr(),
-                const SizedBox(height: 10),
-                const DividerWidget(),
-                const SizedBox(height: 10),
-                EasyAutocomplete(
+    } else {
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 5),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Theme.of(context).colorScheme.onPrimary,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    _appBloc.tenChuyen!,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 10),
+                  const DividerWidget(),
+                  const SizedBox(height: 10),
+                  EasyAutocomplete(
                     controller: _qrDataController,
                     onChanged: _onSearchChanged,
                     suggestions: _results,
@@ -219,11 +199,12 @@ class _MainPageState extends State<MainPage> {
                         horizontal: 10,
                       ),
                       focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5),
-                          borderSide: const BorderSide(
-                            color: Colors.purple,
-                            style: BorderStyle.solid,
-                          )),
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(
+                          color: Colors.purple,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
                     ),
                     suggestionBuilder: (data) {
                       return Container(
@@ -240,91 +221,84 @@ class _MainPageState extends State<MainPage> {
                           ),
                         ),
                       );
-                    }),
-                const SizedBox(height: 10),
-                ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.camera),
-                    label: const Text('Quét mã'))
-              ],
-            ),
-          ),
-          const SizedBox(height: 10),
-          _loading
-              ? LoadingWidget(height: 200)
-              : _data == null
-                  ? const SizedBox.shrink()
-                  : SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                        itemCount: scannedProducts.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                            title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                showInfoXe(
-                                    "Tên", scannedProducts[index].tenDongXe),
-                                const SizedBox(height: 10),
-                                showInfoXe(
-                                    "Model", scannedProducts[index].tenChiTiet),
-                                const SizedBox(height: 10),
-                                showInfoXe("Loại xe",
-                                    scannedProducts[index].tenLoaiXe),
-                                const SizedBox(height: 10),
-                                if (scannedProducts[index].ngay != null)
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      showInfoXe(
-                                        "Ngày",
-                                        scannedProducts[index].ngay.toString(),
-                                      ),
-                                      const SizedBox(height: 10),
-                                    ],
-                                  ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-          const SizedBox(height: 10),
-          _data == null || _loading
-              ? const SizedBox.shrink()
-              : Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 50,
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).primaryColor,
-                    ),
-                    onPressed: _onSave,
-                    icon: Icon(
-                      Feather.tablet,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                    label: Text(
-                      _ab.isNhapKho
-                          ? (_data!.nhapXuatKhoId == null
-                              ? "Nhập kho"
-                              : "Huỷ xác nhận")
-                          : (_data!.nhapXuatKhoId == null
-                              ? "Xuất kho"
-                              : "Huỷ xác nhận"),
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontSize: 20,
-                      ),
-                    ),
+                    },
                   ),
-                )
-        ],
-      ),
-    );
+                  const SizedBox(height: 10),
+                  ElevatedButton.icon(
+                    onPressed: _scanQRCode,
+                    icon: const Icon(Icons.camera),
+                    label: const Text('Quét mã'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            _loading
+                ? LoadingWidget(height: 200)
+                : _data == null
+                    ? const SizedBox.shrink()
+                    : Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.all(10),
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        child: Column(
+                          children: [
+                            showInfoXe("Tên", _data!.tenChiTiet),
+                            const SizedBox(height: 10),
+                            showInfoXe("Model", _data!.tenDongXe),
+                            const SizedBox(height: 10),
+                            showInfoXe("Loại xe", _data!.tenLoaiXe),
+                            const SizedBox(height: 10),
+                            // ignore: unnecessary_null_comparison
+                            if (_data!.ngay != null)
+                              // if (_data!.ngay != null)
+                              SizedBox(
+                                child: Column(
+                                  children: [
+                                    showInfoXe("Ngày", _data!.ngay.toString()),
+                                    const SizedBox(height: 10),
+                                  ],
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+            const SizedBox(height: 10),
+            _data == null || _loading
+                ? const SizedBox.shrink()
+                : Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
+                      onPressed: _onSave,
+                      icon: Icon(
+                        FontAwesomeIcons.tablet,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                      label: Text(
+                        _appBloc.isNhapKho
+                            ? (_data!.nhapXuatKhoId == null
+                                ? "Nhập kho"
+                                : "Huỷ xác nhận")
+                            : (_data!.nhapXuatKhoId == null
+                                ? "Xuất kho"
+                                : "Huỷ xác nhận"),
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+          ],
+        ),
+      );
+    }
   }
 }
 
